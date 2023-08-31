4208dac88d7ba3deea6a5e55e0796f6d9c51005f
SeanMcAvoy29 <sean.mcavoy@Kainos.com>
2023-08-31 11:24:38 +0100

    DeliveryEmployeeDao created

---
+++ src/main/java/org/example/db/DeliveryEmployeeDao.java
@@ -0,0 +1,38 @@
+package org.example.db;
+
+import org.example.cli.EmployeeRequest;
+
+import java.sql.*;
+
+public class DeliveryEmployeeDao {
+
+    private final DatabaseConnector databaseConnector = new DatabaseConnector();
+    private EmployeeDAO employeeDao = new EmployeeDAO();
+
+    public int createDeliveryEmployee(EmployeeRequest employeeRequest) throws SQLException {
+        Connection c = databaseConnector.getConnection();
+
+        int employeeId = employeeDao.createEmployee(employeeRequest);
+
+        if(employeeId == -1){
+            return -1;
+        }
+
+        String insertStatement = "INSERT INTO DeliveryEmployee (EmployeeID) VALUES (?);";
+
+        PreparedStatement st = c.prepareStatement(insertStatement, Statement.RETURN_GENERATED_KEYS);
+
+        st.setInt(1, employeeId );
+
+        st.executeUpdate();
+        ResultSet rs = st.getGeneratedKeys();
+
+        if(rs.next())
+            return rs.getInt(1);
+
+        return -1;
+
+    }
+
+
+}
